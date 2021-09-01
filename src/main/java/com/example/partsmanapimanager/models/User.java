9b10c071d24ea71d682b350505f4030ad0fea0b0
Dennis Marchuk <dvmarchuk4@gmail.com>
2021-09-01 00:44:43 -0400

    Unused variable removal

--- src/main/java/com/example/partsmanapimanager/models/User.java
+++ src/main/java/com/example/partsmanapimanager/models/User.java
@@ -19,6 +19,5 @@ public class User {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
         private String userName;
-        //private String partURL;
-        //private String orderedItems;
+
 }
